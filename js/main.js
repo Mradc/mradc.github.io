b6c791adb27877c67bcca3a16554e84292f743b3
Mradc <52400050+Mradc@users.noreply.github.com>
2026-04-28 00:52:18 +0500

    Remove startNextStage from imports in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { ui, toggleSpellbook } from './ui.js';
 import { gameState } from './state.js';
-import { startGame, startNextStage, executePlayerAttack, activateArchon, startEnemyTurn, usePotion, castSpell } from './combat.js';
+import { startGame, executePlayerAttack, activateArchon, startEnemyTurn, usePotion, castSpell } from './combat.js';
 import { toggleShop } from './shop.js';
 
 ui.btnMain.addEventListener('click', () => executePlayerAttack(false));
@@ -15,10 +15,11 @@ ui.btnCloseSpellbook.addEventListener('click', () => toggleSpellbook(false));
 ui.btnPotHeal.addEventListener('click', () => usePotion('heal'));
 ui.btnPotElixir.addEventListener('click', () => usePotion('elixir'));
 
-ui.btnNextStage.addEventListener('click', () => startNextStage());
+// Кнопки магазина
 ui.btnOpenShop.addEventListener('click', () => toggleShop(true));
 ui.btnCloseShop.addEventListener('click', () => toggleShop(false));
 
+// Главное меню
 ui.btnStart.addEventListener('click', startGame);
 ui.btnRestart.addEventListener('click', () => { 
     ui.over.classList.add('hidden'); ui.menu.classList.remove('hidden'); 
